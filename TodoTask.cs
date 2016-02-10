93f264689c39b3b0ac178a9f4ab5b098ca579ec4
Duc Nguyen <nnduc@smartdev.vn>
2016-02-10 10:13:43 +0700

    initial project

---
+++ TodoTask.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todo
+{
+    public class TodoTask
+    {
+        public TodoTask()
+        {
+        }
+    }
+}
